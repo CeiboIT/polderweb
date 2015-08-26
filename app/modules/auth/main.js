1ce509390f817e5c2fa71cc7a713bb87fa258288
epotignano <epotignano@gmail.com>
2015-08-26 17:48:11 -0300

    Login refactors, need to finish the refactors for have a landing and continue from this root state from modules/home

--- app/modules/auth/main.js
+++ app/modules/auth/main.js
@@ -1,23 +1,30 @@
 angular.module('polderweb.auth', ['angular-flash.service', 'angular-flash.flash-alert-directive'])
   .config(function ($stateProvider) {
     $stateProvider
-        .state('login', {
+
+        .state('auth', {
+            url: '/access',
+            abstract: true,
+            template: '<div ui-view=""></div>'
+        })
+
+        .state('auth.login', {
         url: '/login',
         templateUrl: 'app/modules/auth/login/login.html',
         controller: 'LoginCtrl as ctrl'
         })
 
-        .state('register', {
+        .state('auth.register', {
             url: '/register',
             templateUrl: 'app/modules/auth/register/register.html',
             controller: 'RegisterController as ctrl'
         })
 
-      .state('logout', {
+      .state('auth.logout', {
         url: '/logout',
         controller: function ($state, authService) {
             authService.logout();
-            $state.go('login');
+            $state.go('auth.login');
           }
       });
   });
