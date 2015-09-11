91b7eb21354c0833ecf0d33d71f8f3a1be33a59d
epotignano <epotignano@gmail.com>
2015-09-11 19:33:04 -0300

    added gender in create aanhef view.

--- app/modules/admin/aanhef/create-aanhef/create-aanhef-controller.js
+++ app/modules/admin/aanhef/create-aanhef/create-aanhef-controller.js
@@ -1,9 +1,12 @@
 angular.module('polderweb')
 	.controller('createAanhefCtrl',
-		function($scope, $state,Aanhef,authService){
+		function($scope, $state,Aanhef,authService, geslachtOptions){
          if(authService.getToken()==null){
            $state.go('auth.login');
          }else{
+
+             $scope.geslachtOptions = geslachtOptions;
+
 		 $scope.addAanhef = function (form) {
 	        $scope.submitted = true;
 	        if (form.$valid) {
