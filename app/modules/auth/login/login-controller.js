5547909f26320e1839fec5d446a8799b341e59f6
maticaputti <matias.caputti@gmail.com>
2015-09-24 10:36:12 -0300

    Added cookie for user data

--- app/modules/auth/login/login-controller.js
+++ app/modules/auth/login/login-controller.js
@@ -1,6 +1,6 @@
 //use the array syntax or $injector for define a the controller function with his dependencies, or it will break when minified.
 
-var LoginCtrl = ['User', '$state','$stateParams', 'flash','homeState',function (User,$state, $stateParams, flash, homeState) {
+var LoginCtrl = ['User', '$state','$stateParams', '$cookies', 'flash', 'homeState',function (User,$state, $stateParams, $cookies, flash, homeState) {
     var ctrl = this;
 
     ctrl.suggestions = {};
@@ -19,29 +19,37 @@ var LoginCtrl = ['User', '$state','$stateParams', 'flash','homeState',function (
             User.getUser(ctrl.auth.username).then(function(data){
                 console.log(data);
                 if(!data) {
-                    flash.error = 'Invalid username'
+                    flash.error = 'Invalid username';
                     ctrl.suggestions = {
                         register: true
-                    }
+                    };
                 } else {
-                    if(data.Passwrd == ctrl.auth.password) {
+                    if(data.Passwrd === ctrl.auth.password) {
+                        //save cookies
+                        $cookies.put('username', data.Username);
+                        $cookies.put('bedrijf', data.Bedrijf);
+                        $cookies.put('token', data.Token);
+
+                        console.log('Cookie username: ', $cookies.get('username'));
+                        console.log('Cookie bedrijf: ', $cookies.get('bedrijf'));
+                        console.log('Cookie token: ', $cookies.get('token'));
                         $state.go(homeState);
                     } else {
-                        flash.error = 'Invalid password'
+                        flash.error = 'Invalid password';
                         ctrl.suggestions = {
                             forgot: true
-                        }
+                        };
                     }
                 }
             });
 
 //        }
-    };
+    }
 
     angular.extend(ctrl,{
         auth: model,
         login: login
-    })
+    });
 }];
 
 angular.module('polderweb.auth')
